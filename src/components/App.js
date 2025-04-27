7969809293a6879cf1c2f5549848b5c8ea542e7a
mudassiralam7 <mudassiralam7@gmail.com>
2025-04-27 07:43:18 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import ChildComponent1 from "./ChildComponent1";
 import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
-  const[selectedOption, setSelectedOption] = useState()
+  const[selectedOption, setSelectedOption] = useState("")
   return (
     <div className="parent">
        <h1>Parent Component</h1>
        <ChildComponent1 setSelectedOption = {setSelectedOption}/>
        <ChildComponent2   setSelectedOption = {setSelectedOption}/>
-       <p>Selected Option : {selectedOption}</p>
+       <p>Selected Option: {selectedOption}</p>
     </div>
   )
 }
