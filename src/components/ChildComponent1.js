13a09dd03756700ef8e9af0f4e9531e392e36bd7
mudassiralam7 <mudassiralam7@gmail.com>
2025-04-27 07:28:31 +0530

    first

---
+++ src/components/ChildComponent1.js
@@ -0,0 +1,10 @@
+import React from "react";
+const ChildComponent1 = ({selectedOption, setSelectedOption})=>{
+    return(
+        <div>
+            <h2>Child Component 1</h2>
+            <button onClick={()=>setSelectedOption("Option 1")}>Option 1</button>
+        </div>
+    )
+}
+export default ChildComponent1
---
+++ src/components/ChildComponent2.js
@@ -0,0 +1,10 @@
+import React from "react";
+const ChildComponent2 = ({selectedOption, setSelectedOption})=>{
+    return(
+        <div>
+            <h2>Child Component 2</h2>
+            <button onClick={()=>setSelectedOption("Option 2")}>Option 2</button>
+        </div>
+    )
+}
+export default ChildComponent2
